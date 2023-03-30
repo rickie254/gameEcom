33d98b55642a15fb51f726b45bc7d63713e75aca
rickie maweu <rickie.maweu@student.moringaschool.com>
2023-03-30 12:32:47 +0300

    updating the price

--- src/pages/home-page/home-page.jsx
+++ src/pages/home-page/home-page.jsx
@@ -8,7 +8,7 @@ const games = [
     image: "/game-covers/forza_5.jpeg",
     title: "Forza Horizon 5",
     genres: ["Race", "Simulator", "Open world"],
-    price: 19.5,
+    price: 59.99,
     video: "https://www.youtube.com/embed/FYH9n37B7Yw",
     id: 1,
     description:
@@ -19,7 +19,7 @@ const games = [
     title: "Battlefield 2042",
     genres: ["Action", "Shooter", "War"],
     video: "https://www.youtube.com/embed/ASzOzrB-a9E",
-    price: 21.8,
+    price: 59.99,
     id: 2,
     description:
       "Battlefield™ 2042 — is a first-person shooter that brings the series back to legendary large-scale battles. In a chaotic future, adapt and thrive on an ever-changing battlefield with your squad and an arsenal of the latest technology.",
@@ -29,7 +29,7 @@ const games = [
     title: "Life is Strange True Colors",
     genres: ["Deep plot", "Protagonist"],
     video: "https://www.youtube.com/embed/b6CkzwVAr0M",
-    price: 27,
+    price: 59.99,
     id: 3,
     description:
       "Alex Chen hides from everyone his 'curse' - the supernatural ability to read the strong emotions of others and influence them. But when her brother dies - allegedly in an accident - Alex uses her to find out the truth.",
@@ -39,7 +39,7 @@ const games = [
     title: "Grand Theft Auto V",
     genres: ["Open world", "Action"],
     video: "https://www.youtube.com/embed/QkkoHAzjnUs",
-    price: 8.4,
+    price: 59.99,
     id: 4,
     description:
       "Grand Theft Auto V for PC allows players to explore the iconic world of Los Santos and Blaine County in resolutions up to 4k and beyond at 60 frames per second.",
@@ -49,7 +49,7 @@ const games = [
     title: "Tom Clancy's Rainbow Six Siege",
     video: "https://www.youtube.com/embed/6wlvYh0h63k",
     genres: ["Tactics", "Shooter"],
-    price: 5,
+    price: 59.99,
     id: 5,
     description:
       "Tom Clancy's Rainbow Six – is a continuation of the acclaimed first-person shooter developed by the studio Ubisoft Montreal.",
@@ -59,7 +59,7 @@ const games = [
     title: "Assassin’s Creed Valhalla",
     genres: ["Parkour", "War", "Открытый мир"],
     video: "https://www.youtube.com/embed/ssrNcwxALS4",
-    price: 17,
+    price: 59.99,
     id: 6,
     description:
       "Assassin’s Creed Valhalla — is a multiplatform action/RPG video game developed by Ubisoft Montreal and published by Ubisoft. It is the twelfth installment in the Assassin's Creed series.",
